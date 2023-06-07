c9cdf1e28d12b0b4ca380135dff45aac240c0416
Antonio Gorgulho <antonioSgorgulho@gmail.com>
2023-06-07 15:59:49 +0100

    Add minor improvements to code

--- app/helpers/applicants_helper.rb
+++ app/helpers/applicants_helper.rb
@@ -1,2 +1,12 @@
 module ApplicantsHelper
+  def applicant_sort_options_for_select
+    [
+      ['Application Date Ascending', 'created_at-asc'],
+      ['Application Date Descending', 'created_at-desc']
+    ]
+  end
+
+  def job_options_for_select(account_id)
+    Job.where(account_id: account_id).order(:title).pluck(:title, :id)
+  end
 end
